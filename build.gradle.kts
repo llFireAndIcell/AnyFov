43c1d941722826f7e0fe3f2f33e9c27e07f65b28
llFireAndIcell <llfireandicell@gmail.com>
2023-02-11 20:39:45 -0500

    Changed build file name, slightly reworded readme

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ blossom {
 version = mod_version
 group = "me.fireandice"
 base {
-    archivesName.set("$mod_name ($platform)")
+    archivesName.set(mod_name)
 }
 loom {
     noServerRunConfigs()
